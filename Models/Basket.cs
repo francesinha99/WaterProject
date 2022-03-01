520e4c31284798653805eaa571ea9107eecb3a9e
michaelpiscione <piscionemichael@gmail.com>
2022-02-28 21:10:58 -0700

    Feb 28th Videos
    Inheritance, Virtual, Sessions, Services

--- Models/Basket.cs
+++ Models/Basket.cs
@@ -8,7 +8,7 @@ namespace WaterProject.Models
     {
         public List<BasketLineItem> Items { get; set; } = new List<BasketLineItem>();
 
-        public void AddItem (Project proj, int qty)
+        public virtual void AddItem (Project proj, int qty)
         {
             BasketLineItem line = Items
                 .Where(p => p.Project.ProjectId == proj.ProjectId)
@@ -29,6 +29,16 @@ namespace WaterProject.Models
             }
         }
 
+        public virtual void RemoveItem(Project proj)
+        {
+            Items.RemoveAll(x => x.Project.ProjectId == proj.ProjectId);
+        }
+
+        public virtual void ClearBasket()
+        {
+            Items.Clear();
+        }
+        
         public double CalculateTotal()
         {
             double sum = Items.Sum(x => x.Quantity * 25);
@@ -37,8 +47,6 @@ namespace WaterProject.Models
         }
 
     }
-
- 
 
     public class BasketLineItem
     {
